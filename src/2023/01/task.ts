dedad243ad8c3b51496c1c2e0e3daf20326a565e
Edvard Borovskij <funalien@gmail.com>
2023-12-01 10:21:27 +0200

    aoc2023-1

---
+++ src/2023/01/task.ts
@@ -0,0 +1,9 @@
+const text = await (Bun.file("src/2023/01/input.txt").text()); 
+const values = text.split('\n');
+const extractedNumbers = values.map((x) => x.match(/[0-9]/g));
+// console.log(extractedNumbers);
+const combinedNumbers = extractedNumbers.map((x) => x != null && x.length != 0 ? x[0]+x[x?.length-1] : [null]);
+// console.log(combinedNumbers);
+const sumNumbers = combinedNumbers.reduce((acc, val) => acc += +val, 0);
+// console.log(sumNumbers);
+Bun.write("src/2023/01/output.txt", sumNumbers.toString());
